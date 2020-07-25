67ab98768bcba6f6f420310872445b2d131677f3
Volomymyr Fedchyshyn <Volodymyr.Fedchyshyn@McKesson.com>
2020-07-25 11:27:15 +0300

    finish task 1.2

--- task1/index1.2.js
+++ task1/index1.2.js
@@ -1,1 +1,30 @@
-console.log("Hello node js2");
+const csv = require("csvtojson");
+const fs = require("fs");
+
+const csvFilePath = "./csv/nodejs-hw1-ex1.csv";
+const txtFilePath = "./csv/nodejs-hw1-ex1.txt";
+
+fs.writeFile(txtFilePath, "", (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
+
+csv({
+  checkType: true,
+})
+  .fromFile(csvFilePath)
+  .subscribe(
+    (resp) => {
+      fs.appendFile(txtFilePath, JSON.stringify(resp) + "\n", (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
--- task1/index1.3.js
+++ task1/index1.3.js
@@ -1,1 +1,11 @@
-console.log("Hello node js3");
+const csv = require("csvtojson");
+const fs = require('fs');
+
+const csvFilePath = './csv/nodejs-hw1-ex1.xlsx'
+// Convert a csv file with csvtojson
+
+csv({output:"line"})
+.fromString(csvFilePath)
+.subscribe((csvLine)=>{ 
+  console.log(csvLine);
+})
