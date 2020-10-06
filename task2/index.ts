2c700c739ada0688c229f0060a4e8902733f193e
Volomymyr Fedchyshyn <Volodymyr.Fedchyshyn@McKesson.com>
2020-10-06 21:51:02 +0300

    removed pakage.lock

--- task2/index.ts
+++ task2/index.ts
@@ -2,6 +2,27 @@ import * as express from 'express';
 import routes from './src/routes/routes';
 import * as bodyParser from 'body-parser';
 
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('favoritecolors', 'postgres', 123456, {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+var Colors = sequelize.define('colors', {
+  colorid: Sequelize.Number,
+  colorname: Sequelize.String
+})
+
+sequelize.sync().success(function() {
+  Colors.create({
+    colorid: 5,
+    colorname: 'yellow'
+  }).success(function(sdepold) {
+    console.log(sdepold.values);
+  })
+})
+
 const app = express();
 const PORT = 3000;
 
